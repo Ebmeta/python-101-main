fdcdeaec4adbc7ad513cb91b336b3f8ee3b415fd
Наталия <intrutina@mail.ru>
2024-12-06 11:03:17 +0700

    06-07

---
+++ python-101-main/06_intro-to-programming/06_01_pseudocode.py
@@ -0,0 +1,14 @@
+# Your cat just had kittens! Now you want to put up an ad to give them
+# to your friends. You'll need to save all names of the kittens,
+# confirm that they each of them is cute, and show a message that
+# that kitten is ready for adoption.
+#
+# Break this task up into a couple of steps of pseudocode
+# and write the pseudocode below in code comments.
+# You don't need to write any functional code, just map out the steps.
+
+# 1. создать список с именами котят
+# 2.добавить к имени котенка прилагательное
+# 3. вывести имя с прилагательным котенка , + готов стать новым членом семьи.
+
+
---
+++ python-101-main/07_variables-and-types/07_04_multiple_improve.py
@@ -0,0 +1,7 @@
+# Improve the variable assignment shown below.
+# Why should you change it?
+
+#color, year = "red", 1900
+color = 'red'
+year = 1900
+# разные типы str and int
